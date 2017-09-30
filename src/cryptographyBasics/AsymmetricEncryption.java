20510cc1897e85e359923eb9e88d99c33b9c9495
yymartin <yoan.martin@epfl.ch>
2017-09-30 18:03:55 +0200

    Replace old version of asymmetric encryption to permit first version of blind signature implementation
    Old implementation using the Cipher java library is replaced by a
    personal implementation of RSA. First version of blind signature is
    based on RSA which is not optimal, should be improved in the future

--- src/cryptographyBasics/AsymmetricEncryption.java
+++ src/cryptographyBasics/AsymmetricEncryption.java
@@ -1,83 +1,67 @@
 package cryptographyBasics;
 
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.math.BigInteger;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Random;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-
-public class AsymmetricEncryption {
-	
-	//The key is public
-	public static byte[] encrypt(String clearText, Key key){
-		Cipher encryptorAlgorithm;
-		byte[] encryptedByte = null;
-		try {
-			encryptorAlgorithm = Cipher.getInstance("RSA");
-			encryptorAlgorithm.init(Cipher.ENCRYPT_MODE, key);
-			encryptedByte = encryptorAlgorithm.doFinal(clearText.getBytes());
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return encryptedByte;
+public class AsymmetricEncryption {	
+	public static BigInteger encrypt(String message, RSAPublicKey key) {
+		BigInteger m = new BigInteger(message.getBytes());
+		BigInteger e = key.getPublicExponent();
+		BigInteger N = key.getModulus();
+		
+		return m.modPow(e, N);	
 	}
 	
-	//The key is private
-	public static String decrypt(byte[] cipherText, Key key){
-		Cipher decryptorAlgorithm;
-		byte[] decryptedByte = null;
-		try {
-			decryptorAlgorithm = Cipher.getInstance("RSA");
-			decryptorAlgorithm.init(Cipher.DECRYPT_MODE, key);
-			decryptedByte = decryptorAlgorithm.doFinal(cipherText);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return new String(decryptedByte);
+	public static String decrypt(BigInteger cipherText, RSAPrivateKey key) {
+		BigInteger d = key.getPrivateExponent();
+		BigInteger N = key.getModulus();
+		
+		return new String(cipherText.modPow(d, N).toByteArray());
 	}
 	
 	//Implementation of Sign(ssk, msg) (page 6)
-	public static byte[] sign(PrivateKey secretSigningKey, String message) {
-		byte[] signature = encrypt(message, secretSigningKey);
+	public static BigInteger sign(RSAPrivateKey key, BigInteger message) {
+		BigInteger d = key.getPrivateExponent();
+		BigInteger N = key.getModulus();
 		
-		return signature;
+		return message.modPow(d, N);	
 	}
 	
 	//Implementation of SigVerify(svk, msg, sig) (page 6)
-	public static boolean signatureVerification(PublicKey secretVerificationKey, String message, byte[] signature) {
-		String clearText = decrypt(signature, secretVerificationKey);
+	public static boolean signatureVerification(RSAPublicKey key, String message, BigInteger signature) {
+		BigInteger e = key.getPublicExponent();
+		BigInteger N = key.getModulus();
+		
+		String clearText = new String(signature.modPow(e, N).toByteArray());
 		
 		return clearText.equals(message);
+	}
+
+	public static BigInteger blind(String message, RSAPublicKey publicKey, BigInteger r) {
+		BigInteger messageAsInteger = new BigInteger(message.getBytes());
+		BigInteger e = publicKey.getPublicExponent();
+		BigInteger N = publicKey.getModulus();
+
+		return messageAsInteger.multiply(r.modPow(e, N)).mod(N);
+	}
+	
+	public static BigInteger unblind(BigInteger signature, RSAPrivateKey privateKey, BigInteger r) {
+		BigInteger N = privateKey.getModulus();
+		BigInteger rInverse = r.modInverse(N);
+		
+		return signature.multiply(rInverse).mod(N);
+	}
+
+	
+	public static BigInteger generateRForBlindSignature(BigInteger mod) {
+		BigInteger r;
+		Random rand = new Random();
+		do {
+			r = new BigInteger(512, rand);
+		} while(r.gcd(mod) == BigInteger.valueOf(1));
+		
+		return r;
 	}
 }
--- src/cryptographyBasicsTest/AsymmetricEncryptionTest.java
+++ src/cryptographyBasicsTest/AsymmetricEncryptionTest.java
@@ -2,7 +2,10 @@ package cryptographyBasicsTest;
 
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
 import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 import org.junit.Test;
 
@@ -15,8 +18,11 @@ public class AsymmetricEncryptionTest {
 		String message = "This is a test message!";
 		KeyPair keyPair = MyKeyGenerator.generateAssymetricKey();
 		
-		byte[] cipherText = AsymmetricEncryption.encrypt(message, keyPair.getPublic());
-		String clearText = AsymmetricEncryption.decrypt(cipherText, keyPair.getPrivate());
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		
+		BigInteger cipherText = AsymmetricEncryption.encrypt(message, publicKey);
+		String clearText = AsymmetricEncryption.decrypt(cipherText, privateKey);
 		
 		assertEquals(message, clearText);	
 	}
@@ -25,8 +31,29 @@ public class AsymmetricEncryptionTest {
 	public void testSignatureVerification() {
 		String message = "This is a message to sign!";
 		KeyPair keyPair = MyKeyGenerator.generateAssymetricKey();
-		byte[] signature = AsymmetricEncryption.sign(keyPair.getPrivate(), message);
 		
-		assertTrue(AsymmetricEncryption.signatureVerification(keyPair.getPublic(), message, signature));
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		
+		BigInteger signature = AsymmetricEncryption.sign(privateKey, new BigInteger(message.getBytes()));
+		
+		assertTrue(AsymmetricEncryption.signatureVerification(publicKey, message, signature));
+	}
+	
+	@Test
+	public void testBlindSignature() {
+		String message = "This is a blind test message!";
+		KeyPair keyPair = MyKeyGenerator.generateAssymetricKey();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		
+		BigInteger r = AsymmetricEncryption.generateRForBlindSignature(publicKey.getModulus());
+
+		BigInteger newMessage = AsymmetricEncryption.blind(message, publicKey, r);
+		BigInteger signature = AsymmetricEncryption.sign(privateKey, newMessage);
+		
+		BigInteger newSignature = AsymmetricEncryption.unblind(signature, privateKey, r);
+				
+		assertTrue(AsymmetricEncryption.signatureVerification(publicKey, message, newSignature));
 	}
 }
